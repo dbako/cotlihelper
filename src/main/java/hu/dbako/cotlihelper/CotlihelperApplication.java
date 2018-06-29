6eb2f44f9965ddba9522f2afc65024dab0dceebf
dbako <bakodani@gmail.com>
2018-06-29 14:45:11 +0200

    Add files via upload
    initial commit, src is directly from start.spring.io

---
+++ src/main/java/hu/dbako/cotlihelper/CotlihelperApplication.java
@@ -0,0 +1,12 @@
+package hu.dbako.cotlihelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotlihelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotlihelperApplication.class, args);
+	}
+}
